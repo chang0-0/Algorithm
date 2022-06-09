a6442d15fe76d4a1275e63c89b57a2880e922357
chang0-0 <bigyoung8375@naver.com>
2022-06-09 16:24:41 +0900

    complete

---
+++ 동적계획법 부수기/Main_11053_가장긴증가하는부분수열.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_11053_가장긴증가하는부분수열 {
+	static Integer memo[];
+	static int arr[];
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/11053.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int A = Integer.parseInt(br.readLine()); 
+		memo = new Integer[A];
+		arr = new int[A];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<A; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+	
+		Arrays.sort(arr);
+		
+		for(int i=0; i<A; i++) {
+			DP(i);
+		}
+			
+		Arrays.sort(memo);
+		
+		System.out.print(memo[A-1]);
+	} // End of main
+
+	private static int DP(int N) {
+
+		if(memo[N] == null) {
+			memo[N] = 1;
+			
+			for(int i=N-1; i>=0; i--) {
+				// 현재의 값이 이전의 값 보다 클 경우,
+				if(arr[i] < arr[N]) {
+					// 이전의 값에 +1을 함
+					memo[N] = Math.max(memo[N], DP(i)+1);					
+				}
+			}
+		}
+		
+		return memo[N];
+	} // End of DP
+	
+} // End of Main class
