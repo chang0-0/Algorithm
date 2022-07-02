f49069efe88a32a68a525b125793b19fda6388bc
chang0-0 <bigyoung8375@naver.com>
2022-07-02 13:06:29 +0900

    complete

--- Solved_ac_CLASS2/Main_15829_Hashing.java
+++ Solved_ac_CLASS2/Main_15829_Hashing.java
@@ -1,19 +1,20 @@
-import java.util.*;
 import java.io.*;
 
 public class Main_15829_Hashing {
+	private static final int M = 1234567891;
+	
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("res/input_bj_15829.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int L = Integer.parseInt(br.readLine());
-		String str = br.readLine();
+		char ch[] = br.readLine().toCharArray();
 		
-		
-		
-		
-		
-		
-		
-	}
-}
+		long result = 0;
+		long pow = 1;
+		for(int i=0; i<L; i++) {
+			result += (ch[i] - 'a'+1) * pow % M;
+			pow = pow * 31 % M;
+		}
+		System.out.println( result % M );
+	} // End of main
+} // End of Main class
