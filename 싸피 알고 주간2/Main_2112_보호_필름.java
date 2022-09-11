ef99cbf26dcce174274cee8c0e89ab5d74066211
bigyoung8375 <bigyoung8375@naver.com>
2022-09-11 19:41:29 +0900

    complete

--- 싸피 알고 주간2/Main_2112_보호_필름.java
+++ 싸피 알고 주간2/Main_2112_보호_필름.java
@@ -7,11 +7,8 @@ import java.io.*;
 // 성능검사를 통과할 수 있는 약품의 최소 투입 횟수이다. 약품을 투입하지 않고도 성능검사를 통과하는 경우에는 0을 출력한다.
 // for (int[] arr : tempArr) System.out.println(Arrays.toString(arr));
 public class Main_2112_보호_필름 {
-    static int D, W, K;
+    static int D, W, K, result;
     static int[][] arr;
-    static int[][] tempArr;
-    static int result;
-
     static int[] isVisited;
 
     public static void main(String[] args) throws Exception {
@@ -35,79 +32,79 @@ public class Main_2112_보호_필름 {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < W; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
-                    tempArr[i][j] = arr[i][j];
                 }
             }
 
-            // for (int[] num : arr) System.out.println(Arrays.toString(num));
-
-            if (searching()) { // 먼저 가능한지 한번 체크
-                sb.append(0).append('\n');
-                continue;
-            }
-
-            DFS(0);
+            DFS(0, 0);
             sb.append(result).append('\n');
+
         }
 
         bw.write(sb.toString());
         bw.close();
     } // End of main
 
-    private static void DFS(int depth) {
+    private static void DFS(int depth, int index) {
 
-        if(depth == K) {
+        // 약품이 투여되지 않았을 때와 한번이라도 투여됬을 때도 searching시작 하기위해 최소 조건을 줌
+        if (depth <= K) {
+            // 가지치기 조건.
+            // 지금까지 최소 약품투여횟수와 현재 투여된 약품투여횟수(depth)가 같으면 더 이상 searching할 필요가 없음
+            if(depth >= result) {
+                return;
+            }
 
-            return;
+            if (searching()) {
+                int count = 0;
+                for (int num : isVisited) {
+                    if (num != 0) {
+                        count++;
+                    }
+                }
+
+                result = Math.min(result, count);
+            }
+
+            if (depth == K) {
+                return;
+            }
         }
 
-        for (int i = 0; i < D; i++) {
+        for (int i = index; i < D; i++) {
             if (isVisited[i] == 2) continue;
-
-            isVisited[i]++;
-            change(i, isVisited[i], depth);
-            DFS(depth + 1);
-            isVisited[i]--;
+            isVisited[i] = 1; // isVisited가 1이면 1로 채우고, 2이면 0으로 채움.
+            DFS(depth + 1, i + 1);
+            isVisited[i] = 2;
+            DFS(depth + 1, i + 1);
+            isVisited[i] = 0;
         }
-
 
     } // End of DFS
-
-    private static void change(int index, int num, int depth) {
-
-        if (num == 1) {
-            Arrays.fill(tempArr[index], 1);
-        } else {
-            Arrays.fill(tempArr[index], 0);
-        }
-
-        for (int[] arr : tempArr) System.out.println(Arrays.toString(arr));
-        System.out.println("========================================");
-
-
-
-        if(searching()) {
-            System.out.println(depth);
-            result = Math.min(result, depth);
-        }
-
-        copy();
-    } // End of change
 
     private static boolean searching() {
         for (int i = 0; i < W; i++) {
             boolean check = false;
-
             int zeroCount = 0;
             int oneCount = 0;
+
             for (int j = 0; j < D; j++) {
-                if (arr[j][i] == 0) {
-                    zeroCount++;
-                    oneCount = 0;
-                } else {
+
+                if (isVisited[j] == 0) {
+                    if (arr[j][i] == 0) {
+                        zeroCount++;
+                        oneCount = 0;
+                    } else if (arr[j][i] == 1) {
+                        oneCount++;
+                        zeroCount = 0;
+                    }
+                } else if (isVisited[j] == 1) {
                     oneCount++;
                     zeroCount = 0;
+                } else if (isVisited[j] == 2) {
+                    zeroCount++;
+                    oneCount = 0;
                 }
+
 
                 // 둘 중 하나라도 K를 찍으면 중지.
                 if (zeroCount == K || oneCount == K) {
@@ -124,18 +121,9 @@ public class Main_2112_보호_필름 {
         return true;
     } // End of searching
 
-    private static void copy() {
-        for (int i = 0; i < D; i++) {
-            for (int j = 0; j < W; j++) {
-                tempArr[i][j] = arr[i][j];
-            }
-        }
-    } // End of copy
-
     private static void init() {
         arr = new int[D][W];
-        tempArr = new int[D][W];
         isVisited = new int[D];
-        result = K+2;
+        result = K+1;
     } // End of init
 } // End of Main class
