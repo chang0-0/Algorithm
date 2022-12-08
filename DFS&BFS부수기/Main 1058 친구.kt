81809d0d7ddb6217c8c87aea0e90bf348bac15b4
bigyoung8375 <bigyoung8375@naver.com>
2022-12-08 23:37:55 +0900

    complete

---
+++ DFS&BFS부수기/Main 1058 친구.kt
@@ -0,0 +1,16 @@
+package `DFS&BFS 부수기`
+
+import java.util.*
+import java.io.*
+
+private var N = 0
+
+fun main() {
+    val path = "C:\\Users\\Samsung\\Desktop\\코틀린 알고리즘\\src\\main\\kotlin\\DFS&BFS 부수기\\res\\1058.txt"
+    val br = BufferedReader(File(path).bufferedReader())
+
+    N = br.readLine().toInt()
+
+
+
+} // End of main
