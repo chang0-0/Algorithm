4ac391459aaca55a4512bddc9670e84a11df5dad
chang0-0 <bigyoung8375@naver.com>
2023-06-23 17:58:46 +0900

    BOJ 16168 Solve

--- 오일러 부수기/Main 16168 퍼레이드.kt
+++ 오일러 부수기/Main 16168 퍼레이드.kt
@@ -5,7 +5,6 @@ import java.io.BufferedWriter
 import java.io.File
 import java.io.OutputStreamWriter
 import java.util.*
-import kotlin.system.exitProcess
 
 /*
     민원을 받지 않으면서 모든 구간을 지나도록 작성
@@ -18,10 +17,9 @@ private lateinit var bw: BufferedWriter
 // variables
 private var V = 0
 private var E = 0
-private const val INF = Int.MAX_VALUE
 
 private lateinit var adjacencyList: MutableList<MutableList<Int>>
-private lateinit var isVisited: Array<IntArray>
+private lateinit var isVisited: BooleanArray
 
 fun main() {
     val path = "C:\\Users\\bigyo\\Desktop\\알고리즘\\KotlinAlgo\\src\\main\\kotlin\\오일러_경로_부수기\\16168.txt"
@@ -29,36 +27,58 @@ fun main() {
     bw = BufferedWriter(OutputStreamWriter(System.out))
     val sb = StringBuilder()
 
-    input()
-    for (i in 1..V) {
-        DFS(i, i, 0)
+    if (input() == "NO") {
+        bw.write("NO")
+        bw.close()
+        return
     }
 
-    bw.write("NO")
+    var count = 0
+    for (i in 1..V) {
+        if (isVisited[i]) continue
+        DFS(i)
+        count++
+        if (count > 1) break
+    }
+
+    if (count != 1) sb.append("NO")
+    else sb.append("YES")
+
+    bw.write(sb.toString())
     bw.close()
 } // End of main
 
-private fun DFS(node: Int, num: Int, count: Int) {
-    if (count == V) {
-        bw.write("YES")
-        exitProcess(0)
-    }
+private fun DFS(node: Int) {
+    if (isVisited[node]) return
+    isVisited[node] = true
 
-    adjacencyList[node].forEach { nextNode ->
-        if (isVisited[node][nextNode] != 0 || isVisited[nextNode][node] != 0) {
-            isVisited[node][nextNode] = isVisited[nextNode][node]
-            isVisited[nextNode][node] = num
-            DFS(nextNode, node, count + 1)
-        }
+    adjacencyList[node].forEach {
+        DFS(it)
     }
 } // End of DFS
 
-private fun input() {
+private fun check(degree: IntArray): String {
+    var odd = 0
+    for (i in 1..V) {
+        if (degree[i] % 2 != 0) {
+            odd++
+        }
+    }
+
+    if (odd == 0 || odd == 2) {
+        return "YES"
+    } else {
+        return "NO"
+    }
+} // End of check
+
+private fun input(): String {
     var st = StringTokenizer(br.readLine())
     V = st.nextToken().toInt() // 지점의 개수
     E = st.nextToken().toInt() // 연결 구간의 개수
 
-    isVisited = Array(V + 1) { IntArray(V + 1) }
+    isVisited = BooleanArray(V + 1)
+    val degree = IntArray(V + 1)
 
     // 단방향 그래프
     adjacencyList = ArrayList()
@@ -72,6 +92,9 @@ private fun input() {
         val v = st.nextToken().toInt()
         adjacencyList[u].add(v)
         adjacencyList[v].add(u)
-
+        degree[u]++
+        degree[v]++
     }
+
+    return check(degree)
 } // End of input
