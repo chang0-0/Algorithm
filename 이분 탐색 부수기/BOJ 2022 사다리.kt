0cd23e0ce31cbd535a5e7bbd14c458743ffab300
chang0-0 <bigyoung8375@naver.com>
2023-07-16 13:31:52 +0900

    BOJ 2022 Solve

--- 이분 탐색 부수기/BOJ 2022 사다리.kt
+++ 이분 탐색 부수기/BOJ 2022 사다리.kt
@@ -3,8 +3,11 @@ package BOJ_2022_사다리
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 // input
 private lateinit var br: BufferedReader
@@ -14,11 +17,12 @@ private lateinit var sb: StringBuilder
 private var X = 0.0
 private var Y = 0.0
 private var C = 0.0
+private var ans = 0.0
 
 fun main() {
     val path = "C:\\Users\\bigyo\\Desktop\\알고리즘\\KotlinAlgo\\src\\main\\kotlin\\BOJ_2022_사다리\\res.txt"
     br = BufferedReader(File(path).bufferedReader())
-    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     sb = StringBuilder()
 
     input()
@@ -30,22 +34,27 @@ fun main() {
 } // End of main
 
 private fun solve() {
-    val left: Double = 0.0
-    val right: Double = Math.min(X, Y)
-
-
+    binarySearch(0.0, X.coerceAtMost(Y))
+    sb.append("%.3f".format(ans))
 } // End of solve
 
-private fun binarySearch(low: Double, high: Double): Double {
+private fun binarySearch(low: Double, high: Double) {
+    if (high - low < 0.001) {
+        ans = (low + high) / 2.0
+        return
+    }
+    val mid: Double = (low + high) / 2.0
 
+    val a: Double = sqrt(X.pow(2.0) - mid.pow(2.0))
+    val b: Double = sqrt(Y.pow(2.0) - mid.pow(2.0))
+    val cal: Double = (a * b) / (a + b)
 
-    if (high - low >= 0.001) return -1.0
-    val mid = (low + high) / 2
-
-
-    return 0.0
+    return if (cal >= C) {
+        binarySearch(mid, high)
+    } else {
+        binarySearch(low, mid)
+    }
 } // End of binarySearch
-
 
 private fun input() {
     val st = StringTokenizer(br.readLine())
