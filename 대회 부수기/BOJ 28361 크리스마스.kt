a412022d39a46890860112a497350cb84f996cb5
chang0-0 <bigyoung8375@naver.com>
2023-07-27 00:09:51 +0900

    BOJ 28361 push

---
+++ 대회 부수기/BOJ 28361 크리스마스.kt
@@ -0,0 +1,27 @@
+import java.util.*
+import java.io.*
+
+// input
+private lateinit var br : BufferedReader
+private lateinit var sb : StringBuilder
+
+// variables
+
+fun main() {
+    val path = ""
+    br = BufferedReader(File(path).bufferedReader())
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    sb = StringBuilder()
+
+    bw.write(sb.toString())
+    bw.close()
+} // End of main
+
+private fun solve() {
+
+} // End of solve
+
+private fun input() {
+
+} // End of input
+
