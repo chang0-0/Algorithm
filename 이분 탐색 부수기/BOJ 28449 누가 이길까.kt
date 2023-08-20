b895b94b7c7991df9125aae0ce1b4cb272ecbace
chang0-0 <bigyoung8375@naver.com>
2023-08-21 01:36:06 +0900

    BOJ 28449 modify

--- 이분 탐색 부수기/BOJ 28449 누가 이길까.kt
+++ 이분 탐색 부수기/BOJ 28449 누가 이길까.kt
@@ -40,13 +40,30 @@ fun main() {
 private fun solve(): String {
     val sb = StringBuilder()
 
+    var win = 0
+    var draw = 0
+    var lose = 0
+
     val hiSize = HIArr.size
     val arcSize = ARCArr.size
     for (i in 0 until hiSize) {
-        binarySearch(0, arcSize - 1, HIArr[i])
+        val target = HIArr[i]
+        val idx = binarySearch(0, arcSize - 1, target)
+        println("idx : $idx")
+
+        val midValue = ARCArr[idx] // 찾은 중앙값
+        if (midValue == target) {
+            draw++
+        } else if (midValue < target) {
+            win += (arcSize / 2) + 1
+            lose += (arcSize / 2) - 1
+        } else {
+            win += (arcSize / 2) - 1
+            lose += (arcSize / 2) + 1
+        }
     }
 
-
+    sb.append(win).append(' ').append(lose).append(' ').append(draw)
     return sb.toString()
 } // End of solve()
 
@@ -57,14 +74,21 @@ private fun binarySearch(low: Int, high: Int, find: Int): Int { // 인덱스를 
 
     // 인덱스를 이분탐색해서 나랑 가장 가까운 값을 찾아야함
     val mid = (low + high) / 2
-    if (ARCArr[mid] > find) {
-        val temp = binarySearch()
+    if (ARCArr[mid] < find) {
+        val temp = binarySearch(low, mid - 1, find)
 
+        when (temp) {
+            -1 -> {
+                return mid
+            }
+
+            else -> {
+                return temp
+            }
+        }
     } else {
-
+        return binarySearch(mid + 1, high, find)
     }
-
-
 } // End of binarySearch
 
 private fun input() {
@@ -85,6 +109,6 @@ private fun input() {
         }
     }
 
-    println(HIArr.sort())
-    println(ARCArr.sort())
+    HIArr.sort()
+    ARCArr.sort()
 } // End of input()
