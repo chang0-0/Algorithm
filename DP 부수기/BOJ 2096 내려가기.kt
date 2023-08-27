0445c229200f1a88af1190d05a0add5af05ca8aa
chang0-0 <bigyoung8375@naver.com>
2023-08-27 13:16:39 +0900

    BOJ 2096 Solve with Kotlin

--- DP 부수기/BOJ 2096 내려가기.kt
+++ DP 부수기/BOJ 2096 내려가기.kt
@@ -14,7 +14,6 @@ private lateinit var br: BufferedReader
 
 // variables
 private var N = 0
-private lateinit var arr: Array<IntArray>
 private lateinit var maxMemo: Array<IntArray>
 private lateinit var minMemo: Array<IntArray>
 
@@ -32,66 +31,53 @@ fun main() {
 private fun solve(): String {
     val sb = StringBuilder()
 
-    maxMemo[1][0] = arr[1][0]
-    minMemo[1][0] = arr[1][0]
-
-    maxMemo[1][1] = arr[1][1]
-    minMemo[1][1] = arr[1][1]
-
-    maxMemo[1][2] = arr[1][2]
-    minMemo[1][2] = arr[1][2]
-    DP()
-
     sb.append(max(maxMemo[N][0], max(maxMemo[N][1], maxMemo[N][2]))).append(' ')
         .append(min(minMemo[N][0], min(minMemo[N][1], minMemo[N][2])))
-
     return sb.toString()
 } // End of solve()
 
-private fun DP() {
-    for (i in 2..N) {
-        // 0 max
-        maxMemo[i][0] = maxMemo[i - 1][0] + arr[i][0]
-        maxMemo[i][1] = maxMemo[i - 1][0] + arr[i][1]
+private fun DP(i: Int, first: Int, second: Int, third: Int) {
+    // 0 max
+    maxMemo[i][0] = maxMemo[i - 1][0] + first
+    maxMemo[i][1] = maxMemo[i - 1][0] + second
 
-        // 0 min
-        minMemo[i][0] = minMemo[i - 1][0] + arr[i][0]
-        minMemo[i][1] = minMemo[i - 1][0] + arr[i][1]
+    // 0 min
+    minMemo[i][0] = minMemo[i - 1][0] + first
+    minMemo[i][1] = minMemo[i - 1][0] + second
 
-        // 1 max
-        // 기존의 값이 최대인지, 새로운 새로값이 최대인지 확인해서 갱신
-        maxMemo[i][0] = max(maxMemo[i][0], maxMemo[i - 1][1] + arr[i][0])
-        maxMemo[i][1] = max(maxMemo[i][1], maxMemo[i - 1][1] + arr[i][1])
-        maxMemo[i][2] = maxMemo[i - 1][1] + arr[i][2]
+    // 1 max
+    // 기존의 값이 최대인지, 새로운 새로값이 최대인지 확인해서 갱신
+    maxMemo[i][0] = max(maxMemo[i][0], maxMemo[i - 1][1] + first)
+    maxMemo[i][1] = max(maxMemo[i][1], maxMemo[i - 1][1] + second)
+    maxMemo[i][2] = maxMemo[i - 1][1] + third
 
-        // 1 min
-        minMemo[i][0] = min(minMemo[i][0], minMemo[i - 1][1] + arr[i][0])
-        minMemo[i][1] = min(minMemo[i][1], minMemo[i - 1][1] + arr[i][1])
-        minMemo[i][2] = minMemo[i - 1][1] + arr[i][2]
+    // 1 min
+    minMemo[i][0] = min(minMemo[i][0], minMemo[i - 1][1] + first)
+    minMemo[i][1] = min(minMemo[i][1], minMemo[i - 1][1] + second)
+    minMemo[i][2] = minMemo[i - 1][1] + third
 
 
-        // 2 max
-        maxMemo[i][1] = max(maxMemo[i][1], maxMemo[i - 1][2] + arr[i][1])
-        maxMemo[i][2] = max(maxMemo[i][2], maxMemo[i - 1][2] + arr[i][2])
+    // 2 max
+    maxMemo[i][1] = max(maxMemo[i][1], maxMemo[i - 1][2] + second)
+    maxMemo[i][2] = max(maxMemo[i][2], maxMemo[i - 1][2] + third)
 
-        // 2 min
-        minMemo[i][1] = min(minMemo[i][1], minMemo[i - 1][2] + arr[i][1])
-        minMemo[i][2] = min(minMemo[i][2], minMemo[i - 1][2] + arr[i][2])
-    }
+    // 2 min
+    minMemo[i][1] = min(minMemo[i][1], minMemo[i - 1][2] + second)
+    minMemo[i][2] = min(minMemo[i][2], minMemo[i - 1][2] + third)
 } // End of DP()
 
 private fun input() {
     N = br.readLine().toInt()
-    arr = Array(N + 1) { IntArray(3) }
+    maxMemo = Array(N + 1) { IntArray(3) }
+    minMemo = Array(N + 1) { IntArray(3) }
 
     for (i in 1..N) {
         StringTokenizer(br.readLine()).run {
-            for (j in 0 until 3) {
-                arr[i][j] = nextToken().toInt()
-            }
+            val first = nextToken().toInt()
+            val second = nextToken().toInt()
+            val third = nextToken().toInt()
+
+            DP(i, first, second, third)
         }
     }
-
-    maxMemo = Array(N + 1) { IntArray(3) }
-    minMemo = Array(N + 1) { IntArray(3) }
 } // End of input()
