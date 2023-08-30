2afdec9354aa9b63ce813106c5fb3d120e648474
chang0-0 <bigyoung8375@naver.com>
2023-08-31 00:44:27 +0900

    BOJ 9252 Solve with Kotlin

--- LCS 부수기/BOJ 9252 LCS 2.kt
+++ LCS 부수기/BOJ 9252 LCS 2.kt
@@ -15,7 +15,9 @@ private var s2 = ""
 private var N = 0
 private var M = 0
 
-private lateinit var memo: Array<IntArray>
+private lateinit var memo: Array<Array<Memo>>
+
+private data class Memo(var cnt: Int = -1, var str: String = "")
 
 fun main() {
     val path = "C:\\Users\\bigyo\\Desktop\\알고리즘\\KotlinAlgo\\src\\main\\kotlin\\BOJ_9252\\res.txt"
@@ -31,30 +33,38 @@ fun main() {
 private fun solve(): String {
     val sb = StringBuilder()
 
-    println("s1 : $s1 , s2 : $s2")
-
-    LCS(N, M)
-
-    memo.forEach {
-        println(it.contentToString())
+    val temp = LCS(N, M)
+    if (temp.cnt == 0) {
+        sb.append(0)
+    } else {
+        sb.append(temp.cnt).append('\n').append(temp.str)
     }
 
     return sb.toString()
 } // End of solve()
 
-private fun LCS(n: Int, m: Int): Int {
-    println("LCS($n, $m)")
+private fun LCS(n: Int, m: Int): Memo {
+    if (n == 0 || m == 0) return Memo(0, "")
 
-    if (n == 0 || m == 0) return 0
-
-    if (memo[n][m] != -1) return memo[n][m]
+    if (memo[n][m].cnt != -1) return memo[n][m]
 
     if (s1[n - 1] == s2[m - 1]) {
         // 같을 때
-        memo[n][m] = LCS(n - 1, m - 1) + 1
+        val temp = LCS(n - 1, m - 1)
+        memo[n][m].cnt = temp.cnt + 1
+        memo[n][m].str += temp.str + s1[n - 1]
     } else {
         // 다를 때
-        memo[n][m] = LCS(n - 1, m).coerceAtLeast(LCS(n, m - 1))
+        val temp1 = LCS(n - 1, m)
+        val temp2 = LCS(n, m - 1)
+
+        if (temp1.cnt > temp2.cnt) {
+            memo[n][m].cnt = temp1.cnt
+            memo[n][m].str = temp1.str
+        } else {
+            memo[n][m].cnt = temp2.cnt
+            memo[n][m].str = temp2.str
+        }
     }
 
     return memo[n][m]
@@ -68,5 +78,5 @@ private fun input() {
     N = s1.length
     M = s2.length
 
-    memo = Array(N + 1) { IntArray(M + 1) { -1 } }
+    memo = Array(N + 1) { Array(M + 1) { Memo() } }
 } // End of input()
