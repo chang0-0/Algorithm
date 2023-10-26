4ec8453013bde1c162b4678450fa4f7bd13ccf4b
chang0-0 <bigyoung8375@naver.com>
2023-10-27 00:31:42 +0900

    BOJ 3190 modify push with Java

--- 자료구조 부수기/BOJ_3190_뱀.java
+++ 자료구조 부수기/BOJ_3190_뱀.java
@@ -1,7 +1,8 @@
 package BOJ_3190;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BOJ_3190_뱀 {
@@ -14,9 +15,8 @@ public class BOJ_3190_뱀 {
     private static int N, K, L;
 
     private static int[][] map;
-    private static int[] dirX = {-1, 1, 0, 0};
-    private static int[] dirY = {0, 0, -1, 1};
-    private static Coordinate[] apples;
+    private static int[] dirX = {0, 1, 0, -1};
+    private static int[] dirY = {1, 0, -1, 0};
     private static Direction[] directions;
 
     private static class Coordinate {
@@ -53,25 +53,60 @@ public class BOJ_3190_뱀 {
     private static String solve() {
         StringBuilder sb = new StringBuilder();
 
+        int time = 0;
+        int directionIdx = 0;
+        int direction = 0;
+        int headX = 1, headY = 1; // 처음 시작 머리가 있는 곳
+        Deque<Coordinate> snake = new LinkedList<>();
+        snake.offer(new Coordinate(headX, headY));
+        map[headX][headY] = 2;
 
+        for (; ; ) {
+            time++;
 
+            int nextX = headX + dirX[direction];
+            int nextY = headY + dirY[direction];
 
+            if (!isAbleCheck(nextX, nextY)) break;
+
+            if (map[nextX][nextY] != 1) {
+                Coordinate tail = snake.pollLast();
+                map[tail.x][tail.y] = 0;
+            }
+
+            snake.offer(new Coordinate(nextX, nextY));
+            map[nextX][nextY] = 2;
+
+            headX = nextX;
+            headY = nextY;
+
+            if (directionIdx < directions.length && directions[directionIdx].time == time) {
+                if (directions[directionIdx].dir == 'L') {
+                    direction = (direction + 3) % 4;
+                } else {
+                    direction = (direction + 1) % 4;
+                }
+                directionIdx++;
+            }
+        }
+
+        sb.append(time);
         return sb.toString();
     } // End of solve()
+
+    private static boolean isAbleCheck(int nextX, int nextY) {
+        return nextX >= 1 && nextX <= N && nextY >= 1 && nextY <= N && map[nextX][nextY] != 2;
+    } // End of isAbleCheck
 
     private static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
-        map = new int[N][N];
+        map = new int[N + 1][N + 1];
 
         StringTokenizer st;
-        apples = new Coordinate[K];
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            apples[i] = new Coordinate(
-                    Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken())
-            );
+            map[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())] = 1;
         }
 
         L = Integer.parseInt(br.readLine());
