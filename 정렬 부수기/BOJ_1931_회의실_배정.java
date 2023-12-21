4f08553d9ce8f09c62b4cc74e5c372a7f5a414d4
chang0_0 <bigyoung8375@naver.com>
2023-12-21 18:47:19 +0900

    BOJ 1931 push Java

---
+++ 정렬 부수기/BOJ_1931_회의실_배정.java
@@ -0,0 +1,32 @@
+package BOJ_1931;
+
+import java.io.*;
+
+public class BOJ_1931_회의실_배정 {
+
+    // input
+    private static BufferedReader br;
+
+    // variables
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream(""));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input();
+
+        bw.write(solve());
+        bw.close();
+    } // End of main()
+
+    private static String solve() {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    } // End of solve()
+
+    private static void input() throws IOException {
+
+    } // End of input()
+} // End of Main class
