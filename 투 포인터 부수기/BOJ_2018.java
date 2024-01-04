5455a64de2dce25d0d5d603eedc97293306e814a
chang0_0 <bigyoung8375@naver.com>
2024-01-04 18:41:54 +0900

    BOJ 2018 Solve with Java

---
+++ 투 포인터 부수기/BOJ_2018.java
@@ -0,0 +1,51 @@
+package BOJ_2018;
+
+import java.io.*;
+
+public class BOJ_2018 {
+
+    // https://www.acmicpc.net/problem/2018
+    // input
+    private static BufferedReader br;
+
+    // variables
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("C:\\Users\\bigyo\\Desktop\\알고리즘\\JavaAlgorithm\\src\\BOJ_2018\\res.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input();
+
+        bw.write(solve());
+        bw.close();
+    } // End of main()
+
+    private static String solve() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(twoPointer());
+        return sb.toString();
+    } // End of solve()
+
+    private static int twoPointer() {
+        int left = 1, right = 1, sum = 0, ans = 0;
+        while (right <= N + 1) {
+            if (sum < N) {
+                sum += right++;
+            } else if (sum == N) {
+                ans++;
+                sum += right++;
+            } else {
+                sum -= left++;
+            }
+        }
+
+        return ans;
+    } // End of twoPointer()
+
+    private static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
+    } // End of input()
+} // End of Main class
