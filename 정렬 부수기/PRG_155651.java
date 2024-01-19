a8aea9549e354f402f9dc4ea2e82524ea2db3d91
chang0_0 <bigyoung8375@naver.com>
2024-01-20 04:18:50 +0900

    PRG_155651 Solve with Java

--- 정렬 부수기/PRG_155651.java
+++ 정렬 부수기/PRG_155651.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public static PriorityQueue<Time> pQue;
+    public static Time[] times;
     
     public static class Time implements Comparable<Time> {
         int start;
@@ -14,37 +15,35 @@ class Solution {
         
         @Override
         public int compareTo(Time o) {
-            if(end == o.end) {
-                return start - o.start;
-            }
             return end - o.end;
         }
-        
-        @Override
-        public String toString() {
-            return "Time{" + start + ", " + end + "} \n";
-        } // End of toString() 
     } // End of Time class
     
     public int solution(String[][] book_time) {
         
-        // 정렬 -> 끝나는 시간을 기준으로 정렬
         input(book_time);
         int ans = 0;
+    
+        Arrays.sort(times, Comparator.comparingInt(o -> o.start));
         
-        while(!pQue.isEmpty()) {
-            System.out.println(pQue.poll());
+        for(Time time : times) {
+            if(!pQue.isEmpty() && time.start >= pQue.peek().end) {
+                pQue.poll();
+            } else {
+                ans++;
+            }
+            pQue.offer(time);
         }
 
-        
-        
         return ans;
     } // End of solution()
     
     public void input(String[][] book_time) {
         pQue = new PriorityQueue<>();
+        int N = book_time.length;
+        times = new Time[N];
         
-        for(int i=0; i<book_time.length; i++) {
+        for(int i=0; i<N; i++) {
             int startTime = 0;
             int endTime = 0;
             
@@ -73,7 +72,7 @@ class Solution {
                     endTime = Integer.parseInt(sb.toString());
                 }
             }
-            pQue.offer(new Time(startTime, endTime));
+            times[i] = new Time(startTime, endTime);
         } 
     } // End of input()
 } // End of Solution 
