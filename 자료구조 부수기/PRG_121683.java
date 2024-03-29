5871d7a7f30b124ec34c4c01ff58743ad830149e
chang0_0 <bigyoung8375@naver.com>
2024-03-29 17:06:39 +0900

    PRG_121686 Solve with Java

--- 자료구조 부수기/PRG_121683.java
+++ 자료구조 부수기/PRG_121683.java
@@ -1,46 +1,71 @@
 import java.util.*;
 
-class Solution {
-    public String solution(String input_string) {        
-        char[] chArr = input_string.toCharArray();
-        int n = chArr.length;
-        
-        TreeMap<Character, Integer> map = new TreeMap<>();
-                
-        boolean flag = false;
-        char ch = ' ';
-        for(int i=0; i<n - 1; i++) {
-            ch = chArr[i];
-            
-            if(flag && chArr[i + 1] != ch) {
-                flag = false;
-                map.put(ch, map.getOrDefault(ch, 0) + 1);
-            } else if(!flag && chArr[i + 1] != ch) {
-                map.put(ch, map.getOrDefault(ch, 0) + 1);
-            }
-             
-            if(ch == chArr[i + 1]) {
-                flag = true;
-            }
-        }
-        
-        if(ch != chArr[n - 1]) {    
-            map.put(chArr[n - 1], map.getOrDefault(chArr[n - 1], 0) + 1);
-        } else {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);           
+public class Solution {
+    
+    public static class Program implements Comparable<Program> {
+        int score;
+        int calledTime;
+        int finishedTime;
+
+        public Program(int score, int calledTime, int finishedTime) {
+            this.score = score;
+            this.calledTime = calledTime;
+            this.finishedTime = finishedTime;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for(char c : map.keySet()) {
-            if(map.get(c) >= 2) {
-                sb.append(c);
+        @Override
+        public int compareTo(Program o) {
+            if(score == o.score) {
+                return calledTime - o.calledTime;
             }
+            
+            return score - o.score;
+        }
+    } // End of Program class
+    
+    public static long[] solution(int[][] programs) {
+        int N  = programs.length;
+        
+        Arrays.sort(programs, (o1, o2) -> {
+            if(o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
+            }
+            
+            return Integer.compare(o1[1], o2[1]);
+        });
+        
+        PriorityQueue<Program> waitingQue = new PriorityQueue<>();
+        long[] ans = new long[11];
+        
+        long time = -1;
+        int run = 0;
+        int idx = 0;
+        
+        for(;;) {
+            if(idx == N && waitingQue.isEmpty() && run == 0) {
+                break;
+            }
+            
+            time++;
+            if(run > 0) {
+                run--;
+            }
+            
+            while(idx < N && programs[idx][1] == time) {
+                waitingQue.offer(new Program(programs[idx][0], programs[idx][1], programs[idx][2] ));
+                idx++;
+            }
+            
+            if(run == 0 && !waitingQue.isEmpty()) {
+                Program current = waitingQue.poll();
+                
+                run += current.finishedTime;
+                ans[current.score] += time - current.calledTime;
+            }   
         }
         
-        if(sb.length() == 0) {
-            sb.append("N");
-        }
-                
-        return sb.toString();
+        ans[0] = time;
+        
+        return ans;
     } // End of solution()
 } // End of Solution class
