cd13081030a693b6c1f0982ca2c3397cc358cf94
chang0_0 <bigyoung8375@naver.com>
2024-04-15 10:59:53 +0900

    PRG 250136 Solve with Java

--- DFS&BFS부수기/PRG_250136.java
+++ DFS&BFS부수기/PRG_250136.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public static int[][] land;
-    public static boolean[][] isVisited;
+    public static HashMap<Integer, Integer> indexMap;
     public static int N, M;
-    public static final int[] dirX = {-1, 0, 1, 0}; // 상 우 하 좌
-    public static final int[] dirY = {0, 1, 0, -1};
+    public static int[][] map, oilIndexMap;
+    public static boolean[][] isVisited;
+    
+    public static final int[] dirX = {-1, 1, 0, 0}; // 하 상 좌 우
+    public static final int[] dirY = {0, 0, -1, 1}; // 하 상 좌 우
+    
     
     public static class Coordinate {
         int x;
@@ -20,58 +23,58 @@ class Solution {
     public int solution(int[][] land) {        
         input(land);
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int idx = 2;
+        int mapIdx = 1;
         for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(isVisited[i][j] || land[i][j] == 0) continue;
-                
-                int count = BFS(i, j, idx);
-                map.put(idx++, count);                
+            for(int j=0; j<M; j++) {                
+                if(!isVisited[i][j] && map[i][j] == 1) {
+                    int ret = BFS(i, j, mapIdx);
+                    
+                    indexMap.put(mapIdx++, ret);
+                }
             }
         }
         
         int ans = 0;
-        for(int i=0; i<M; i++) {
-            Set<Integer> set = new HashSet<>();
-            
-            for(int j=0; j<N; j++) {
-                if(land[j][i] > 0) {
-                    set.add(land[j][i]);
+        // 세로로 진행
+        for(int y=0; y<M; y++) {
+            HashSet<Integer> set = new HashSet<>();
+            for(int x=0; x<N; x++) {
+                if(oilIndexMap[x][y] != 0) {
+                    set.add(oilIndexMap[x][y]);
                 }
             }
             
             int sum = 0;
-            for(int num : set) {
-                sum += map.get(num);
+            for(int idx : set) {
+                sum += indexMap.get(idx);
             }
+            
             ans = Math.max(ans, sum);
         }
         
-         
+        
         return ans;
     } // End of solution()
     
-    public int BFS(int x, int y, int idx) {
-        LinkedList<Coordinate> que = new LinkedList<>();
-        int count = 1;
-        
-        que.offer(new Coordinate(x,y));
-        land[x][y] = idx;
-        isVisited[x][y] = true;
+    public int BFS(int x, int y, int mapIdx) {
+        ArrayDeque<Coordinate> que = new ArrayDeque<>();
+        que.offer(new Coordinate(x, y));
+        int count = 0;
         
         while(!que.isEmpty()) {
-            Coordinate current = que.poll();
+            Coordinate cur = que.poll();
+            
+            if(isVisited[cur.x][cur.y]) continue;
+            isVisited[cur.x][cur.y] = true;
+            oilIndexMap[cur.x][cur.y] = mapIdx;
+            count++;
             
             for(int i=0; i<4; i++) {
-                int nextX = dirX[i] + current.x;
-                int nextY = dirY[i] + current.y;
+                int nextX = dirX[i] + cur.x;
+                int nextY = dirY[i] + cur.y;
                 
                 if(!isAbleCheck(nextX, nextY)) continue;
-                isVisited[nextX][nextY] = true;
                 
-                land[nextX][nextY] = idx;
-                count++;
                 que.offer(new Coordinate(nextX, nextY));
             }
         }
@@ -80,14 +83,15 @@ class Solution {
     } // End of BFS()
     
     public boolean isAbleCheck(int nextX, int nextY) {
-        return nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && !isVisited[nextX][nextY] && land[nextX][nextY] == 1;
+        return nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && map[nextX][nextY] == 1;
     } // End of isAbleCheck()
     
     public void input(int[][] land) {
+        map = land;
         N = land.length;
         M = land[0].length;
+        oilIndexMap = new int[N][M];
+        indexMap = new HashMap<>();
         isVisited = new boolean[N][M];
-        
-        this.land = land;
-    } // End of input();
-} //End of Solution class
+    } // End of input()
+} // End of Solution class
