95b7bd58c82a9e1d91edecb42773de3c9c733897
chang0_0 <bigyoung8375@naver.com>
2024-05-18 13:51:44 +0900

    BOJ 1445 Solve with Java

--- DFS&BFS부수기/BOJ_1445.java
+++ DFS&BFS부수기/BOJ_1445.java
@@ -1,13 +1,11 @@
 package BOJ_1445;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.*;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_1445 {
 
+    // https://www.acmicpc.net/problem/1445
     // input
     private static BufferedReader br;
 
@@ -17,6 +15,7 @@ public class BOJ_1445 {
     private static Coordinate start;
     private static final int[] dirX = {-1, 1, 0, 0};
     private static final int[] dirY = {0, 0, -1, 1};
+    private static List<Coordinate> trash;
 
     private static class Coordinate implements Comparable<Coordinate> {
         int x;
@@ -37,7 +36,7 @@ public class BOJ_1445 {
         }
 
         @Override
-        public int compareTo(@NotNull Coordinate o) {
+        public int compareTo(Coordinate o) {
             if (trashCount == o.trashCount) {
                 return sideCount - o.sideCount;
             }
@@ -60,52 +59,65 @@ public class BOJ_1445 {
     private static String solve() {
         StringBuilder sb = new StringBuilder();
 
-        dijkstra();
+        for (Coordinate t : trash) {
+            for (int i = 0; i < 4; i++) {
+                int nextX = dirX[i] + t.x;
+                int nextY = dirY[i] + t.y;
 
-        return sb.toString();
-    } // End of solve()
+                if (!isAbleCheck(nextX, nextY)) continue;
 
-    private static void dijkstra() {
-        PriorityQueue<Coordinate> pque = new PriorityQueue<>();
-        boolean[][] isVisited = new boolean[N][M];
-        int[][] dist = new int[N][M];
-
-
-        int temp = 0;
-        for (int i = 0; i < 4; i++) {
-            int nextX = dirX[i] + start.x;
-            int nextY = dirY[i] + start.y;
-
-            // 주변에 쓰레기가 있으면 +
-            if (isAbleCheck(nextX, nextY, isVisited) && arr[nextX][nextY] == 'g') {
-                temp++;
+                if (arr[nextX][nextY] == '.') {
+                    // 주변을 변경
+                    arr[nextX][nextY] = 'x';
+                }
             }
         }
 
-        dist[start.x][start.y] = temp;
-        pque.offer(new Coordinate(start.x, start.y, 0, temp));
+        int[] ret = dijkstra();
+
+        sb.append(ret[0]).append(' ').append(ret[1]);
+        return sb.toString();
+    } // End of solve()
+
+    private static int[] dijkstra() {
+        PriorityQueue<Coordinate> pque = new PriorityQueue<>();
+        boolean[][] isVisited = new boolean[N][M];
+
+        pque.offer(new Coordinate(start.x, start.y, 0, 0));
 
         while (!pque.isEmpty()) {
             Coordinate cur = pque.poll();
+
+            if (isVisited[cur.x][cur.y]) continue;
+            isVisited[cur.x][cur.y] = true;
 
             for (int i = 0; i < 4; i++) {
                 int nextX = dirX[i] + cur.x;
                 int nextY = dirY[i] + cur.y;
 
-                if (!isAbleCheck(nextX, nextY, isVisited)) continue;
+                if (!isAbleCheck(nextX, nextY)) continue;
+                if (isVisited[nextX][nextY]) continue;
+                // 먼저 주변에 쓰레기가 있는지 파악하자
 
-                isVisited[nextX][nextY] = true;
-
-                if (arr[nextX][nextY] == 'g') {
-                    pque.offer(new Coordinate(nextX, nextY, cur.trashCount + 1, cur.sideCount));
+                char temp = arr[nextX][nextY];
+                if (temp == 'F') {
+                    return new int[]{cur.trashCount, cur.sideCount};
                 }
 
+                if (temp == '.') {
+                    pque.offer(new Coordinate(nextX, nextY, cur.trashCount, cur.sideCount));
+                } else if (temp == 'g') {
+                    pque.offer(new Coordinate(nextX, nextY, cur.trashCount + 1, cur.sideCount));
+                } else if (temp == 'x') {
+                    pque.offer(new Coordinate(nextX, nextY, cur.trashCount, cur.sideCount + 1));
+                }
             }
         }
 
+        return new int[]{0, 0};
     } // End of dijkstra()
 
-    private static boolean isAbleCheck(int nextX, int nextY, boolean[][] isVisited) {
+    private static boolean isAbleCheck(int nextX, int nextY) {
         return nextX >= 0 && nextX < N && nextY >= 0 && nextY < M;
     } // End of isAbleCheck()
 
@@ -113,6 +125,8 @@ public class BOJ_1445 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+        trash = new ArrayList<>();
 
         arr = new char[N][M];
         for (int i = 0; i < N; i++) {
@@ -122,6 +136,8 @@ public class BOJ_1445 {
 
                 if (arr[i][j] == 'S') {
                     start = new Coordinate(i, j);
+                } else if (arr[i][j] == 'g') {
+                    trash.add(new Coordinate(i, j));
                 }
             }
         }
