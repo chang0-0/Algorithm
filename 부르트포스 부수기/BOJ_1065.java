3077fbf716da0af9bd3a2aacda8b2c6ac64a83db
chang0_0 <bigyoung8375@naver.com>
2024-10-12 13:37:15 +0900

    BOJ 1065 Solve with Java

---
+++ 부르트포스 부수기/BOJ_1065.java
@@ -0,0 +1,47 @@
+package BOJ_1065;
+
+import java.io.*;
+
+public class BOJ_1065 {
+
+    // https://www.acmicpc.net/problem/1065
+    // input
+    private static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("C:\\Users\\bigyo\\Desktop\\알고리즘\\JavaAlgorithm\\src\\BOJ_1065\\res.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(solve());
+        bw.close();
+    } // End of main()
+
+    private static String solve() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+
+        int N = Integer.parseInt(br.readLine());
+        if (N < 100) {
+            sb.append(N);
+            return sb.toString();
+        }
+
+        int ans = 99;
+        for (int i = 100; i <= N; i++) {
+            if (check(i)) {
+                ans++;
+            }
+        }
+
+        sb.append(ans);
+        return sb.toString();
+    } // End of solve()
+
+    private static boolean check(int num) {
+        int hundreds = num / 100;
+        int tens = (num / 10) % 10;
+        int units = num % 10;
+        return (hundreds - tens) == (tens - units);
+    } // Enf of check()
+} // End of Main class
