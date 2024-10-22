3dd40ebcaf36d4121e088fb2a17b8c449bd14df5
chang0_0 <bigyoung8375@naver.com>
2024-10-22 13:59:05 +0900

    BOJ 11060 Solve with Java

--- DFS&BFS부수기/BOJ_11060.java
+++ DFS&BFS부수기/BOJ_11060.java
@@ -14,28 +14,24 @@ public class BOJ_11060 {
     // variables
     private static int N;
     private static int[] arr;
-    private static final int INF = Integer.MAX_VALUE / 2;
 
-    private static class Jump implements Comparable<Jump> {
-        int num;
+    private static class Di implements Comparable<Di> {
+        int idx;
         int count;
 
-        private Jump(int num, int count) {
-            this.num = num;
+        private Di(int idx, int count) {
+            this.idx = idx;
             this.count = count;
         }
 
         @Override
-        public int compareTo(Jump o) {
-            return count - o.count;
+        public int compareTo(Di o) {
+            if (o.idx == idx) {
+                return count - o.count;
+            }
+            return o.idx - idx;
         }
-
-
-        @Override
-        public String toString() {
-            return "Jump{" + num + ", " + count + "}\n";
-        }
-    } // End of Jump()
+    } // End of Di class
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("C:\\Users\\bigyo\\Desktop\\알고리즘\\JavaAlgorithm\\src\\BOJ_11060\\res.txt"));
@@ -53,29 +49,30 @@ public class BOJ_11060 {
 
         int ret = BFS();
 
-        if (ret == INF) sb.append(-1);
-        else sb.append(ret);
+        sb.append(ret == Integer.MAX_VALUE ? -1 : ret);
         return sb.toString();
     } // End of solve()
 
     private static int BFS() {
-        PriorityQueue<Jump> que = new PriorityQueue<>();
+        PriorityQueue<Di> que = new PriorityQueue<>();
         int[] memo = new int[N + 1];
-        Arrays.fill(memo, INF);
+        Arrays.fill(memo, Integer.MAX_VALUE);
+
+        que.offer(new Di(0, 0));
         memo[0] = 0;
-        que.offer(new Jump(0, 0));
 
         while (!que.isEmpty()) {
-            Jump cur = que.poll();
+            Di cur = que.poll();
 
-            if (cur.num == N - 1) {
-                memo[cur.num] = Math.min(memo[cur.num], cur.count);
+            if (cur.idx == N) {
+                continue;
             }
 
-            for (int i = cur.num + 1; i <= cur.num + arr[cur.num] && i < N; i++) {
-                if (memo[i] > memo[cur.num] + 1) {
-                    memo[i] = memo[cur.num] + 1;
-                    que.offer(new Jump(i, memo[i]));
+            for (int i = cur.idx; i <= arr[cur.idx] + cur.idx; i++) {
+                if (i > N) break;
+                if (memo[i] > memo[cur.idx] + 1) {
+                    memo[i] = memo[cur.idx] + 1;
+                    que.offer(new Di(i, memo[i]));
                 }
             }
         }
@@ -85,9 +82,9 @@ public class BOJ_11060 {
 
     private static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
