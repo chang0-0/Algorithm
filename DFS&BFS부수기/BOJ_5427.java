2d1056fde1b05b850951f65d9373cc6c74b50138
chang0_0 <bigyoung8375@naver.com>
2024-10-29 20:30:48 +0900

    BOJ 5427 Solve with Java

--- DFS&BFS부수기/BOJ_5427.java
+++ DFS&BFS부수기/BOJ_5427.java
@@ -1,9 +1,7 @@
 package BOJ_5427;
 
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_5427 {
 
@@ -13,51 +11,38 @@ public class BOJ_5427 {
 
     // variables
     private static int N, M;
-    private static Coordinate start;
-    private static ArrayDeque<Coordinate> que;
-    private static boolean[][] isVisited;
     private static char[][] board = new char[1001][1001];
+    private static final int[] dirX = {-1, 0, 1, 0};
+    private static final int[] dirY = {0, 1, 0, -1};
+    private static final String IM = "IMPOSSIBLE";
+    private static List<Coor> fireList;
 
-    private static final int[] dirX = {-1, 1, 0, 0};
-    private static final int[] dirY = {0, 0, -1, 1};
-    private static final String IMPOSSIBLE = "IMPOSSIBLE";
+    private static Coor start;
 
-    private static class Coordinate {
+    private static class Coor {
         int x;
         int y;
         int time;
-        char type;
 
-        private Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        private Coordinate(int x, int y, int time, char type) {
+        private Coor(int x, int y, int time) {
             this.x = x;
             this.y = y;
             this.time = time;
-            this.type = type;
         }
 
         @Override
         public String toString() {
-            return "Coordinate{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    ", time=" + time +
-                    ", type=" + type +
-                    '}';
+            return "Coor{" + x + ", " + y + ", " + time + "}";
         }
-    } // End of Coordinate class
+    } // End of Coor class
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("C:\\Users\\bigyo\\Desktop\\알고리즘\\JavaAlgorithm\\src\\BOJ_5427\\res.txt"));
         br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
             input();
 
             bw.write(solve());
@@ -69,75 +54,77 @@ public class BOJ_5427 {
     private static String solve() {
         StringBuilder sb = new StringBuilder();
 
-        que.offer(new Coordinate(start.x, start.y, 0, 's'));
         int ret = BFS();
-        if(ret == -1) {
-            sb.append(IMPOSSIBLE);
-        } else {
-            sb.append(ret);
-        }
-
-        sb.append('\n');
+        sb.append(ret == -1 ? IM : ret).append('\n');
         return sb.toString();
     } // End of solve()
 
     private static int BFS() {
+        ArrayDeque<Coor> que = new ArrayDeque<>();
+        boolean[][] isVisited = new boolean[N][M];
+        int ans = -1;
+
+        for (Coor t : fireList) {
+            que.offer(t);
+            isVisited[t.x][t.y] = true;
+        }
+        que.offer(start);
+        isVisited[start.x][start.y] = true;
 
         while (!que.isEmpty()) {
-            Coordinate cur = que.poll();
+            Coor cur = que.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextX = dirX[i] + cur.x;
-                int nextY = dirY[i] + cur.y;
+                int nX = dirX[i] + cur.x;
+                int nY = dirY[i] + cur.y;
 
-                if (cur.type == 's') {
-                    if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
+                if (!isAbleCheck(nX, nY)) {
+                    if (cur.time != -1) {
                         return cur.time + 1;
                     }
+                    continue;
                 }
 
-                if (!isAbleCheck(nextX, nextY, isVisited)) continue;
-                if (cur.type == 'f') {
-                    que.offer(new Coordinate(nextX, nextY, cur.time, 'f'));
-                    board[nextX][nextY] = '*';
-                    isVisited[nextX][nextY] = true;
+                if (board[nX][nY] == '#' || isVisited[nX][nY]) continue;
+
+
+                if (cur.time == -1) {
+                    // 불
+                    board[nX][nY] = '*';
+                    que.offer(new Coor(nX, nY, -1));
+                    isVisited[nX][nY] = true;
                 } else {
-                    if (board[nextX][nextY] != '*') {
-                        que.offer(new Coordinate(nextX, nextY, cur.time + 1, 's'));
-                        board[nextX][nextY] = '@';
-                        board[cur.x][cur.y] = '.';
-                        isVisited[nextX][nextY] = true;
-                    }
+                    // 상근이
+                    if (board[nX][nY] == '*') continue;
+                    que.offer(new Coor(nX, nY, cur.time + 1));
+                    isVisited[nX][nY] = true;
                 }
             }
         }
 
-        return -1;
+        return ans;
     } // End of BFS()
 
-    private static boolean isAbleCheck(int nextX, int nextY, boolean[][] isVisited) {
-        return nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && !isVisited[nextX][nextY] && board[nextX][nextY] != '#';
+    private static boolean isAbleCheck(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     } // End of isAbleCheck()
 
     private static void input() throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        que = new ArrayDeque<>();
-        isVisited = new boolean[N][M];
 
+        fireList = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             String temp = br.readLine();
             for (int j = 0; j < M; j++) {
                 board[i][j] = temp.charAt(j);
 
                 if (board[i][j] == '@') {
-                    start = new Coordinate(i, j);
+                    start = new Coor(i, j, 0);
                     board[i][j] = '.';
-                    isVisited[i][j] = true;
                 } else if (board[i][j] == '*') {
-                    que.offer(new Coordinate(i, j, 0, 'f'));
-                    isVisited[i][j] = true;
+                    fireList.add(new Coor(i, j, -1));
                 }
             }
         }
