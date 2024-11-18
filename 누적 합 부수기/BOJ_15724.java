655b6d8a61cb097dac7d430e054b0fe9fcb7b410
chang0_0 <bigyoung8375@naver.com>
2024-11-18 15:10:55 +0900

    BOJ 15724 Solve with Java

---
+++ 누적 합 부수기/BOJ_15724.java
@@ -0,0 +1,68 @@
+package BOJ_15724;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_15724 {
+
+    // https://www.acmicpc.net/problem/15724
+    // input
+    private static BufferedReader br;
+
+    // variables
+    private static int[][] board;
+    private static int N, M, K;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("C:\\Users\\bigyo\\Desktop\\알고리즘\\JavaAlgorithm\\src\\BOJ_15724\\res.txt"));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input();
+
+        bw.write(solve());
+        bw.close();
+    } // End of main()
+
+    private static String solve() throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        int[][] prefixSum = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                prefixSum[i][j] = board[i - 1][j - 1]
+                        + prefixSum[i - 1][j]
+                        + prefixSum[i][j - 1]
+                        - prefixSum[i - 1][j - 1];
+            }
+        }
+
+        K = Integer.parseInt(br.readLine());
+        while (K-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int temp = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+            sb.append(temp).append('\n');
+        }
+
+        return sb.toString();
+    } // End of solve()
+
+    private static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    } // End of input()
+} // End of Main class
