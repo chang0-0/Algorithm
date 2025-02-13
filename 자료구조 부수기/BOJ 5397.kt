936badb945d57a6856dbd5632ff259fc13fea58c
chang0_0 <bigyoung8375@naver.com>
2025-02-13 16:04:32 +0900

    BOJ 5397 Solve with Kotlin

---
+++ 자료구조 부수기/BOJ 5397.kt
@@ -0,0 +1,61 @@
+package BOJ_5397
+
+import java.io.File
+
+// https://www.acmicpc.net/problem/5397
+// input
+private var br = System.`in`.bufferedReader()
+
+// variables
+private var str = ""
+
+fun main() {
+    val path = "C:\\Users\\bigyo\\Desktop\\알고리즘\\KotlinAlgo\\src\\main\\kotlin\\BOJ_5397\\res.txt"
+    br = File(path).bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val T = br.readLine().toInt()
+    repeat(T) {
+        input()
+
+        bw.write(solve())
+    }
+
+    bw.close()
+} // End of main()
+
+private fun solve(): String {
+    val sb = StringBuilder()
+
+    val N = str.length
+    val que = ArrayDeque<Char>()
+    val iter = que.listIterator()
+
+    for (i in 0 until N) {
+        val ch = str[i]
+
+        if (ch == '<') {
+            if (iter.hasPrevious()) iter.previous()
+        } else if (ch == '>') {
+            if (iter.hasNext()) iter.next()
+        } else if (ch == '-') {
+            if (iter.hasPrevious()) {
+                iter.previous()
+                iter.remove()
+            }
+        } else {
+            iter.add(ch)
+        }
+    }
+
+    while (que.isNotEmpty()) {
+        sb.append(que.removeFirst())
+    }
+
+    sb.append('\n')
+    return sb.toString()
+} // End of solve()
+
+private fun input() {
+    str = br.readLine()
+} // End of input()
