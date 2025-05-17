8ac5f7e78ba6e39eef9530ec0ee15738f3ad4c05
chang0_0 <bigyoung8375@naver.com>
2025-05-17 15:55:43 +0900

    PRG 250136 Solve with Java

--- DFS&BFS부수기/PRG_250136.java
+++ DFS&BFS부수기/PRG_250136.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     private static int N, M;
-    private static int[][] land;
+    private static int[][] map;
     private static boolean[][] isVisited;
-    private static final int[] dirX = {-1, 0, 1, 0}; // 상 우 하 좌
-    private static final int[] dirY = {0, 1, 0, -1}; // 상 우 하 좌
-    
+    private static int[] dirX = {-1, 0, 1, 0}; // 상 우 하 좌
+    private static int[] dirY = {0, 1, 0, -1}; 
+    private static HashMap<Integer, Integer> hashMap;
     
     private static class Coordinate {
         int x;
@@ -18,74 +18,69 @@ class Solution {
         }
     } // End of Coordinate class
     
-    public int solution(int[][] land) {
-        int answer = 0;
-        
-        this.land = land;
-        N = land.length;
-        M = land[0].length;
+    public int solution(int[][] land) {        
+        map = land;
+        N = map.length;
+        M = map[0].length;
         isVisited = new boolean[N][M];
-        HashMap<Integer, Integer> landMap = new HashMap<>();
-        int num = 2;
-    
         
+        hashMap = new HashMap<>();
+        int num = 1;
         for(int i=0; i<N; i++) {
             for(int j=0; j<M; j++) {
-                
-                if(land[i][j] == 1 && !isVisited[i][j]) {
-                    int ret = BFS(i, j, num);
-                    landMap.put(num++, ret);
+                if(map[i][j] == 1 && !isVisited[i][j]) {
+                    int count = BFS(i, j, ++num);
+                    hashMap.put(num, count);
                 }
             }
         }
-        
         
         int ans = 0;
         for(int i=0; i<M; i++) {
-            HashSet<Integer> set = new HashSet<>();
+            HashSet<Integer> hashSet = new HashSet<>();
+            int sum = 0;
+            
             for(int j=0; j<N; j++) {
-                if(land[j][i] >= 2) {
-                    set.add(land[j][i]);
+                if(map[j][i] != 0) {
+                    
+                    if(hashSet.add(map[j][i])) {
+                        sum += hashMap.get(map[j][i]);                        
+                    }
                 }
             }
             
-            int sum = 0;
-            for(int temp : set) {
-                sum += landMap.get(temp);
-            }
-            ans = Math.max(sum, ans);
+            ans = Math.max(ans, sum);
         }
-        
         
         return ans;
     } // End of solution()
     
-    public int BFS(int x, int y, int num) {
+    private int BFS(int x, int y, int num) {        
         ArrayDeque<Coordinate> que = new ArrayDeque<>();
-        que.offer(new Coordinate(x, y));
         int count = 1;
-        land[x][y] = num;
-        isVisited[x][y] = true;
+        que.offer(new Coordinate(x, y));
+        map[x][y] = num;
         
         while(!que.isEmpty()) {
             Coordinate cur = que.poll();
             
             for(int i=0; i<4; i++) {
-                int nX = cur.x + dirX[i];
-                int nY = cur.y + dirY[i];
+                int nX = dirX[i] + cur.x;
+                int nY = dirY[i] + cur.y;   
                 
-                if(!isAbleCheck(nX, nY)) continue;
+                if(!check(nX, nY)) continue;
+                
                 que.offer(new Coordinate(nX, nY));
                 isVisited[nX][nY] = true;
-                land[nX][nY] = num;
+                map[nX][nY] = num;
                 count++;
-            }
+            }            
         }
         
         return count;
     } // End of BFS()
     
-    public boolean isAbleCheck(int nX, int nY) {
-        return nX >= 0 && nX < N && nY >= 0 && nY < M && !isVisited[nX][nY] && land[nX][nY] == 1;
-    } // End of isAbleCheck()
+    private boolean check(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M && !isVisited[x][y] && map[x][y] == 1;
+    } // End of check()
 } // End of Solution class
