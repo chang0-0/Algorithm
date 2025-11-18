6f8508bfb45dd9f6d7090f1689f9212a3d4eb4b4
chang0_0 <bigyoung8375@naver.com>
2025-11-18 23:51:36 +0900

    BOJ 43164.java

--- 그래프 부수기/PRG_43164.java
+++ 그래프 부수기/PRG_43164.java
@@ -1,46 +1,51 @@
 import java.util.*;
 
 class Solution {
-    static int N;
-    
-    public List<String> solution(String[][] tickets) {        
+    public List<String> solution(String[][] tickets) {
+        String[] answer = {};
         
-        // 정렬
-        N = tickets.length;
+        int N = tickets.length;
+        int M = 2;
+        
         Arrays.sort(tickets, (o1, o2) -> {
-            return o1[0].compareTo(o2[0]);
+           return o1[0].compareTo(o2[0]); 
         });
         
-        LinkedHashMap<String, LinkedList<String>> adjList = new LinkedHashMap<>();
+        HashMap<String, LinkedList<String>> adjList = new HashMap<>();
         for(int i=0; i<N; i++) {
-            adjList.put(tickets[i][0], new LinkedList<>());
+            String st = tickets[i][0];
+            String ed = tickets[i][1];
+            adjList.computeIfAbsent(st, k -> new LinkedList<>()).offer(ed);
         }
         
-        for(int i=0; i<N; i++) {
-            adjList.get(tickets[i][0]).offer(tickets[i][1]);
-            Collections.sort(adjList.get(tickets[i][0]));
+        for(List<String> list : adjList.values()) {
+            Collections.sort(list);
         }
-                
         
-        Stack<String> stack = new Stack<>();
-        List<String> list = new ArrayList<>();
-        stack.push("ICN");
+        // System.out.println(adjList);
         
-        while(!stack.isEmpty()) {
-            String cur = stack.peek();
+        ArrayDeque<String> que = new ArrayDeque<>();
+        List<String> ansList = new ArrayList<>();
+        // 잊지말자, que는 ArrayDeque를 그냥, stack처럼 활용하기 위해 사용할 뿐임.
+        // 이 문제는 오일러 회로 문제이다.
+        // BFS 문제와 전혀 상관없기 때문에 BFS의 구현 방식을 떠올리면 안된다.
+        
+        que.offer("ICN");
+        
+        while(!que.isEmpty()) {
+            String cur = que.peekLast();
             
-            if(adjList.get(cur) != null && !adjList.get(cur).isEmpty()) {
-                String next = adjList.get(cur).removeFirst();
-            
-                stack.push(next);
+            if(adjList.get(cur) == null || adjList.get(cur).isEmpty()) {
+                ansList.add(que.pollLast());
             } else {
-                list.add(stack.pop());
+                que.offer(adjList.get(cur).pollFirst());
             }
-            
         }
         
-        Collections.reverse(list);        
         
-        return list;
-    } // End of solution()
-} // End of Solution class
+        Collections.reverse(ansList);
+        // System.out.println(ansList);
+        
+        return ansList;
+    } // End of main()
+} // End of Main class
